b0d47f7accf87fc318d1985bd5490052381d15be
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-11-27 18:15:22 -0800

    Added readme for MLiA algorithms

---
+++ machine_learning_in_action/README.md
@@ -0,0 +1,9 @@
+### Follow with me as I work through simple and complex Machine Learning algorithms using Dr. Peter Harrington's book: Machine Learning in Action! 
+
+#### CHAPTER 2: Classifying with k-Nearest Neighbors
+- STARTED: November 27, 2017
+- COMPLETED: ???
+
+#### CHAPTER 3: Splitting Data Sets One Feature at a Time: Decision Trees
+- STARTED: ???
+- COMPLETED: ???
--- machine_learning_in_action/kNN.py
+++ machine_learning_in_action/kNN.py
@@ -1,4 +1,5 @@
 # Tutorial program to apply the k-Nearest Neighbor ML Algorithm
+# All source code is available at www.manning.com/MachineLearningInAction 
 # Credit: MACHINE LEARNING IN ACTION (PETER HARRINGTON)
 
 
@@ -41,3 +42,15 @@ def file2matrix(filename):
     returnMat = zeros((numberOfLines, 3))
     classLabelVector = []
     fr = open(filename)
+
+    # Parse line to a list
+    index = 0
+    for line in fr.readlines():
+        line = line.strip()
+        listFromLine = line.split("\t")
+        returnMat[index, :] = listFromLine[0:3]
+
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+
+    return returnMat, classLabelVector
