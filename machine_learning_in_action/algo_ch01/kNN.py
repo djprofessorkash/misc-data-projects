e7b7e3132233c86aaa51d4e3455b01509683ead6
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-11-29 00:24:49 -0800

    Created main run function

--- machine_learning_in_action/algo_ch01/kNN.py
+++ machine_learning_in_action/algo_ch01/kNN.py
@@ -9,6 +9,7 @@ Credit: MACHINE LEARNING IN ACTION (PETER HARRINGTON)
 
 from numpy import *
 from matplotlib import pyplot as plt
+# from array import array
 import operator
 
 # Function that creates data set from given arrays and labels
@@ -17,14 +18,6 @@ def createDataSet():
     labels = ["A", "A", "B", "B"]
     return group, labels
 
-"""
-To run the createDataSet() function in the Python interpreter, write the following:
-
->>> import kNN                              # NOTE: Imports main file in the interpreter
->>> group, labels = kNN.createDataSet()     # NOTE: Creates data sets from given data
->>> group                                   # NOTE: Displays created data set
->>> labels                                  # NOTE: Displays created labels
-"""
 
 # Function that classifies inputted array based on control dataset
 def classify0(inX, dataSet, labels, k):
@@ -48,17 +41,6 @@ def classify0(inX, dataSet, labels, k):
     sortedClassCount = sorted(classCount.items(), key = operator.itemgetter(1), reverse = True)
     return sortedClassCount[0][0]
 
-"""
-To run the classify0() function in the Python interpreter, write the following:
-
->>> import kNN                                  # NOTE: Imports main file in the interpreter
->>> group, labels = kNN.createDataSet()         # NOTE: Creates data set from given data
->>> kNN.classify0(inX, dataSet, labels, k)      # NOTE: Runs classifier function on dataset
-        - inX:      [0, 0] or other 2x1 array between [0, 0] and [1, 1].
-        - dataSet:  group
-        - labels:   labels
-        - k:        3 
-"""
 
 # Function that converts data from text file into dataset and relative labels 
 def file2matrix(filename):
@@ -84,18 +66,23 @@ def file2matrix(filename):
 
     return returnMat, classLabelVector
 
-"""
-To run the file2matrix() function in the Python interpreter, write the following:
 
->>> import kNN                                                          # NOTE: Imports main file in the interpreter
->>> datingDataMat, datingLabels = kNN.file2matrix("datingTestSet.txt")  # NOTE: Runs file2matrix conversion 
->>> datingDataMat                                                       # NOTE: Displays created data set
->>> datingLabels                                                        # NOTE: Displays created labels
+def main():
+    group, labels = createDataSet()
+    # print(group)
+    # print(labels)
 
->>> from matplotlib import pyplot as plt                    # NOTE: Installs plotting capabilities
->>> from array import array                                 # NOTE: Installs array constructors
->>> fig = plt.figure()                                      # NOTE: Initializes plot
->>> ax = fig.add_subplot(111)                               # NOTE: Creates unit plot in first quadrant
->>> ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2])    # NOTE: Generates scatter data across plot
->>> plt.show()                                              # NOTE: Renders plot
-"""
+    classifier = classify0([0, 0], group, labels, 3)
+    # print(classifier)
+
+    datingDataMat, datingLabels = file2matrix("datingTestSet.txt")
+    # print(datingDataMat)
+    # print(datingLabels[:10])
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2])
+    plt.show()
+
+if __name__ == "__main__":
+    main()
