7b8e5737facece69a12431db13ab9dde1e072944
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-12-06 21:23:19 -0800

    Quit playing games with my heart

--- machine_learning_in_action/algo_ch01/kNN.py
+++ machine_learning_in_action/algo_ch01/kNN.py
@@ -82,7 +82,7 @@ def main():
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2])
-    # ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2], array(datingLabels))
+    # ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2], 15.0 * array(datingLabels), 15.0 * array(datingLabels))
     plt.show()
 
 if __name__ == "__main__":
