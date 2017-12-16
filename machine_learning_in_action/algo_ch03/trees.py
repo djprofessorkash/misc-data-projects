7b984b8b1b5caa15b6e34c3d52d3c20100facbbc
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-12-15 21:46:28 -0800

    Built basic skeleton of decision tree

--- machine_learning_in_action/algo_ch03/trees.py
+++ machine_learning_in_action/algo_ch03/trees.py
@@ -31,6 +31,7 @@ CREDIT:             Machine Learning In Action (Peter Harrington)
 # ====================================================================================
 
 
+import operator as op                       # Library for intrinsic Pythonic mathematical operations
 from math import log                        # Package for performing logarithmic operations
 from time import time as t                  # Package for tracking modular and program runtime
 
@@ -78,8 +79,50 @@ def calculate_Shannon_entropy(dataset):
         info_probability = float(label_counts[key] / num_of_entries)
         Shannon_entropy -= info_probability * log(info_probability, 2)
 
-    print("SHANNON ENTROPY OF SAMPLE DATASET IS: {}\n".format(Shannon_entropy))
+    # print("SHANNON ENTROPY OF SAMPLE DATASET IS: {}\n".format(Shannon_entropy))
     return Shannon_entropy
+
+def split_dataset(dataset, axis, value):
+    split_data = []
+
+    # Iterate through dataset and split into subsets based on unique features
+    for feature_vector in dataset:
+        if feature_vector[axis] == value:
+            reduced_feature_vector = feature_vector[:axis]
+            reduced_feature_vector.extend(feature_vector[axis + 1:])
+            split_data.append(reduced_feature_vector)
+
+    # print("SPLITTED DATA SUBSETS ARE: {}\n".format(split_data))
+    return split_data
+
+def choose_best_feature_to_split_on(dataset):
+    num_of_features = len(dataset[0]) - 1
+    base_entropy = calculate_Shannon_entropy(dataset)
+    best_information_gain = 0.0
+    best_feature = 1
+
+    # Create unique set of data labels to identify best feature to split on
+    for feature in range(num_of_features):
+        feature_list = [sample[feature] for sample in dataset]
+        unique_values = set(feature_list)
+        new_entropy = 0.0
+
+        # Calculate entropy for each feature in dataset
+        for value in unique_values:
+            subset = split_dataset(dataset, feature, value)
+            info_probability = len(subset) / float(len(dataset))
+            new_entropy += info_probability * calculate_Shannon_entropy(subset)
+        
+        # Calculate relative information gain for particular feature
+        information_gain = base_entropy - new_entropy
+
+        # Find best information gain and best feature across all features
+        if (information_gain > best_information_gain):
+            best_information_gain = information_gain
+            best_feature = feature
+    
+    print("BEST FEATURE TO SPLIT ON: {}\nRESPECTIVE BEST INFORMATION GAIN: {}\n".format(best_feature, best_information_gain))
+    return best_feature
  
  
 # ====================================================================================
@@ -94,12 +137,22 @@ def main():
     # Initialize class instance of the decision tree algorithm
     dt = Decision_Tree_Algorithm()
 
-    dataset, labels = create_dataset()
-    calculate_Shannon_entropy(dataset)
+    """ Testing Shannon entropy function """
+    # dataset, labels = create_dataset()
+    # calculate_Shannon_entropy(dataset)
     
+    """ Testing manipulating Shannon entropy levels """
     # dataset[0][-1] = "maybe"
     # print(dataset)
     # calculate_Shannon_entropy(dataset)
+
+    """ Testing split_dataset() function on sample data """
+    # dataset, labels = create_dataset()
+    # split_dataset(dataset, 0, 1)
+
+    """ Testing choose_best_feature_to_split_on() against sample data """
+    dataset, labels = create_dataset()
+    choose_best_feature_to_split_on(dataset)
     return
 
 if __name__ == "__main__":
