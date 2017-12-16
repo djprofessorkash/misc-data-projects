23a7f1ca4e118a489f11433a7633f646bb3d5c88
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-12-15 21:15:48 -0800

    Wrote Shannon entropy and minor dataset functions

--- machine_learning_in_action/algo_ch03/trees.py
+++ machine_learning_in_action/algo_ch03/trees.py
@@ -40,8 +40,46 @@ from time import time as t                  # Package for tracking modular and p
 # ====================================================================================
 
 
-class k_Decision_Tree_Algorithm(object):
-    pass
+class Decision_Tree_Algorithm(object):
+    
+    def __init__(self):
+        pass
+
+
+def create_dataset():
+    dataset =  [[1, 1, "yes"],
+                [1, 1, "yes"],
+                [1, 0, "no"],
+                [0, 1, "no"],
+                [0, 1, "no"]]
+    labels = ["no surfacing", "flippers"]
+
+    print("\nSAMPLE DATA ARE: {}\nSAMPLE LABELS ARE: {}\n".format(dataset, labels))
+    return dataset, labels
+
+def calculate_Shannon_entropy(dataset):
+    num_of_entries = len(dataset)
+    label_counts = dict()
+
+    # Creates dictionary of all possible class types
+    for feature_vector in dataset:
+        current_label = feature_vector[-1]
+
+        if current_label not in label_counts.keys():
+            label_counts[current_label] = 0
+        
+        label_counts[current_label] += 1
+    
+    # Initialize Shannon entropy, the measure of information in a dataset
+    Shannon_entropy = 0.0
+
+    # Calculates Shannon entropy based on product of information types and probabilities
+    for key in label_counts:
+        info_probability = float(label_counts[key] / num_of_entries)
+        Shannon_entropy -= info_probability * log(info_probability, 2)
+
+    print("SHANNON ENTROPY OF SAMPLE DATASET IS: {}\n".format(Shannon_entropy))
+    return Shannon_entropy
  
  
 # ====================================================================================
@@ -54,7 +92,10 @@ def main():
     t0 = t()
 
     # Initialize class instance of the decision tree algorithm
-    kD = k_Decision_Tree_Algorithm()
+    dt = Decision_Tree_Algorithm()
+
+    dataset, labels = create_dataset()
+    calculate_Shannon_entropy(dataset)
     return
 
 if __name__ == "__main__":
