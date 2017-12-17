d998c76b648d65f86b22fc785046c75fbf15d0f3
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-12-17 00:08:39 -0800

    Created file for naïve Bayes classifier algorithm

---
+++ machine_learning_in_action/algo_ch04/bayes.py
@@ -0,0 +1,33 @@
+"""
+NAME:               bayes.py (data_projects/machine_learning_in_action/algo_ch03/)
+
+DESCRIPTION:        Python class application of the Naïve Bayes classifier algorithm.  
+
+                    ???
+
+                    All source code is available at www.manning.com/MachineLearningInAction. 
+
+USE CASE(S):        Nominal values
+
+ADVANTAGE(S):       Works with small amount of data
+                    Handles multiple classes
+
+DISADVANTAGE(S):    Sensitive to how input data is prepared
+
+NOTE:               Original source code is Python 2, but my code is Python 3.
+
+CREDIT:             Machine Learning In Action (Peter Harrington)
+"""
+
+
+# ====================================================================================
+# ================================ IMPORT STATEMENTS =================================
+# ====================================================================================
+
+
+from time import time as t                  # Package for tracking modular and program runtime
+
+
+# ====================================================================================
+# ================================= CLASS DEFINITION =================================
+# ====================================================================================
