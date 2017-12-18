daa7d3db1cb0edcedbe3fa7ffc0821fbdd8bc3dd
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-12-17 16:01:30 -0800

    I think the return vector is working?

--- machine_learning_in_action/algo_ch04/bayes.py
+++ machine_learning_in_action/algo_ch04/bayes.py
@@ -31,3 +31,47 @@ from time import time as t                  # Package for tracking modular and p
 # ====================================================================================
 # ================================= CLASS DEFINITION =================================
 # ====================================================================================
+
+
+def load_data_set():
+    posting_list = [["my", "dog", "has", "flea", "problems", "help", "please"],
+                    ["maybe", "not", "take", "him", "to", "dog", "park", "stupid"],
+                    ["my", "dalmation", "is", "so", "cute", "I", "love", "him"],
+                    ["stop", "posting", "stupid", "worthless", "garbage"],
+                    ["mr", "licks", "ate", "my", "steak", "how", "to", "stop", "him"],
+                    ["quit", "buying", "worthless", "dog", "food", "stupid"]]
+    class_vector = [0, 1, 0, 1, 0, 1]
+
+    print("POSTING LIST IS: {}\nCLASS VECTOR IS: {}\n".format(posting_list, class_vector))
+    return posting_list, class_vector
+
+def create_vocab_list(dataset):
+    vocab_set = set([])
+
+    for document in dataset:
+        vocab_set = vocab_set | set(document)
+
+    print("LIST OF VOCABULARY WORDS IS: {}\n".format(list(vocab_set)))
+    return list(vocab_set)
+
+def convert_word_set_to_vector(vocab_list, input_set):
+    return_vector = [0] * len(vocab_list)
+
+    for word in input_set:
+        if word in vocab_list:
+            return_vector[vocab_list.index(word)] = 1
+        else:
+            print("The word '{}' is not in my vocabulary. ".format(word))
+    
+    print("RETURN VECTOR IS: {}\n".format(return_vector))
+    return return_vector
+
+def main():
+    list_of_posts, list_of_classes = load_data_set()
+    list_of_vocab_words = create_vocab_list(list_of_posts)
+
+    convert_word_set_to_vector(list_of_vocab_words, list_of_posts[0])
+    return
+
+if __name__ == "__main__":
+    main()
