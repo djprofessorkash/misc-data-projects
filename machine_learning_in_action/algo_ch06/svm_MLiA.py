3fbbb8b53a8186c7a0ba6b9841eb528767684a49
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-12-25 16:26:40 -0700

    Created support vector machine file

---
+++ machine_learning_in_action/algo_ch06/svm_MLiA.py
@@ -0,0 +1,8 @@
+def main():
+    # Track starting time of program
+    TIME_I = t()
+
+    return print("Support vector machine class algorithm is done.\n")
+
+if __name__ == "__main__":
+    main()
