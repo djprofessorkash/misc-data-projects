1d3680867782ab12ca833c601052a9e107e12c49
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2018-01-07 16:01:43 -0500

    Created linear regression modeling file

---
+++ machine_learning_in_action/algo_ch08/regression.py
@@ -0,0 +1,22 @@
+"""
+NAME:               regression.py (data_projects/machine_learning_in_action/algo_ch08/)
+
+DESCRIPTION:        Python class application of ???.
+
+                    ???
+
+                    All source code is available at www.manning.com/MachineLearningInAction. 
+
+USE CASE(S):        Numeric and nominal values
+
+ADVANTAGE(S):       Relatively easy to interpret results
+                    Computationally inexpensive
+
+DISADVANTAGE(S):    Poor model for nonlinear data
+
+NOTE:               Original source code is in Python 2, but my code is in Python 3.
+
+CREDIT:             Machine Learning in Action (Peter Harrington)
+"""
+
+
